91ce020ff64763e9dcbab63a11aa0f916ef5dcc7
Tianlin <linjiangyu0702@linjiangyu.com>
2023-05-11 19:30:13 +0800

    none

--- cdn/docs/js/1/saw.js
+++ cdn/docs/js/1/saw.js
@@ -1,5 +1,7 @@
+<script>
 document.body.oncopy = function () {
   swal("复制成功 🎉",
           "若要转载或引用请务必保留原文链接，并申明来源。如果你觉得本仓库不错，那就来 GitHub 给个 Star 吧   --By Org KunTower",
           "success"); 
 };
+</script>
